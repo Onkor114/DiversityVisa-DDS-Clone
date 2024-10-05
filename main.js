13eced6c9702b58f92f656307388a31288171d11
Onkor114 <abdinourahmed114@gmail.com>
2024-10-05 07:20:00 +0300

    updated js file

---
+++ main.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('.input');
+
+button.addEventListener('click', function(){
+    alert("Thanks for your choosing DDS diversity visa program we are welcoming to you more and more 🙋")
+})
